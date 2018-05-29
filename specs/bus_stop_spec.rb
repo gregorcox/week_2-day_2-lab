b8caa21412d31d4cdd68d4681d8c76c7e20c373d
Gregor Cox <gregorcox@gmail.com>
2018-05-29 15:23:31 +0100

    add to queue method added

---
+++ specs/bus_stop_spec.rb
@@ -0,0 +1,32 @@
+require("minitest/autorun")
+require_relative("../bus.rb")
+require_relative("../person.rb")
+require_relative("../bus_stop.rb")
+require("minitest/rg")
+
+class BusStopTest < MiniTest::Test
+
+  def setup
+    @bus_stop1 = BusStop.new("Princes St", [])
+  end
+
+  def test_length_of_queue
+    assert_equal(0, @bus_stop1.length_of_queue())
+  end
+
+  def test_add_to_queue
+    @bus_stop1.add_to_queue(@passenger1)
+    assert_equal(1, @bus_stop1.length_of_queue())
+  end
+
+
+
+
+
+
+
+
+
+
+
+end
